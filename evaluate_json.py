f81b8cdcc57df3b2de4d277716b3b92a26bcf4de
burkse01 <burkse@hawaii.edu>
2023-12-12 19:55:35 -1000

    s

--- evaluate_json.py
+++ evaluate_json.py
@@ -5,23 +5,28 @@ expected_json_directory = "data/receipts/json/expected"
 actual_json_directory = "data/receipts/json/actual"
 receipt_txt_directory = "data/receipts/text"
 
-def find_span(text, entity_text, label):
-    start = text.find(entity_text)
+def find_span(input, label, receipt_text):
+    start = receipt_text.find(input)
     if start == -1:
-        return None
-    end = start + len(entity_text)
-    return {"start": start, "end": end, "label": label}
+        return []
+    end = start + len(input)
+    return [{"start": start, "end": end, "label": label}]
 
-def recursive_entity_parse(json_dict, receipt_text):
-    output = []
-    for element in json_dict:
-        if(type(json_dict[element]) == dict or type(json_dict[element]) == list):
-            output.append(recursive_entity_parse(json_dict[element], receipt_text))
-        elif(type(json_dict[element]) == str):
-            output.append(find_span(json_dict[element], receipt_text, element.upper()))
-        else:
-            output.append(find_span(str(json_dict[element]), receipt_text, element.upper()))         
-    return output
+def recursive_entity_parse(input, label, receipt_text):
+    if(type(input) == dict):
+        output = []
+        for element in input:
+            output = output + (recursive_entity_parse(input[element], element, receipt_text))
+        return output
+    elif(type(input) == list):
+        output = []
+        for element in input:
+            output = output + (recursive_entity_parse(element, '',  receipt_text))
+        return output
+    elif(type(input) == str):
+        return find_span(input, label.upper(), receipt_text)
+    else:
+        return find_span(str(input), label.upper(), receipt_text)
 
 def make_txt_dict(path):
     output = {}
@@ -37,22 +42,25 @@ def make_json_file_dict(path):
     output = {}
     for f in os.scandir(path):
         if(".json" in f.name):
-            file = open(f)
-            json.load(file)
-            key = f.name.replace(".json", "")
-            output[key] = file
-            f.close()
+            try:
+                with open(f, "r") as fp:
+                    data = json.load(fp)
+                key = f.name.replace(".json", "")
+                output[key] = data
+            except:
+                print("Error decoding json")
     return output
 
 receipts = make_txt_dict(receipt_txt_directory)
 expected = make_json_file_dict(expected_json_directory)
 actual = make_json_file_dict(actual_json_directory)
 evaluations = {}
-
+tag_list = ['MECHANT']
 for element in expected:
     if(element in actual and element in receipts):
-        evaluations[element] = Evaluator(recursive_entity_parse(actual[element], receipts[element]),
-                                         recursive_entity_parse(expected[element], receipts[element]),
-                                         tags=['MERCHANT', 'UNITPRICE']).evaluate()
-        print(evaluations[element])
+        actual_parsed = recursive_entity_parse(actual[element], '', receipts[element])
+        expected_parsed = recursive_entity_parse(expected[element], '', receipts[element])
+        print(actual_parsed)
+    evaluator = Evaluator(expected_parsed, actual_parsed, tags=tag_list)
+    print(evaluator.evaluate())
 
